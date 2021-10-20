6797b97fb4719efc681f956a46590ff9bf054dd1
AbiRaditya <abi.radithya@gmail.com>
2021-10-20 16:22:14 +0800

    transaksi penjualan reactive ke produk list

--- client/src/store/actions/penjualanAction.js
+++ client/src/store/actions/penjualanAction.js
@@ -1,74 +1,73 @@
-import {
-  FETCH_CUSTOMERS
-} from "../keys"
+import { FETCH_CUSTOMERS } from "../keys";
+import { getAllProduct } from ".";
 import API from "../../apis/API";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export function setCustomers(payload) {
   return {
     type: FETCH_CUSTOMERS,
-    payload
-  }
+    payload,
+  };
 }
 
 export function fetchCustomers() {
   return async function (dispatch) {
-    const token = await AsyncStorage.getItem("access_token")
+    const token = await AsyncStorage.getItem("access_token");
 
     try {
       const customers = await API({
         method: "GET",
         url: "/customer",
         headers: {
-          access_token: token
-        }
-      })
+          access_token: token,
+        },
+      });
 
-      dispatch(setCustomers(customers.data))
+      dispatch(setCustomers(customers.data));
     } catch (error) {
       console.log(error.response.data);
     }
-  }
+  };
 }
 
 export function addCustomer(payload) {
   return async function (dispatch) {
-    const token = await AsyncStorage.getItem("access_token")
+    const token = await AsyncStorage.getItem("access_token");
 
     try {
       const newCustomer = await API({
         method: "POST",
         url: `/customer`,
         headers: {
-          access_token: token
+          access_token: token,
         },
-        data: payload
-      })
+        data: payload,
+      });
 
       console.log(newCustomer);
     } catch (error) {
       console.log(error.response.data);
     }
-  }
+  };
 }
 
 export function addPenjualan(payload, endPoint) {
   return async function (dispatch) {
-    const token = await AsyncStorage.getItem("access_token")
+    const token = await AsyncStorage.getItem("access_token");
 
     try {
       const newPenjualan = await API({
         method: "POST",
         url: `/penjualan/${endPoint}`,
         headers: {
-          access_token: token
+          access_token: token,
         },
-        data: payload
-      })
-
+        data: payload,
+      });
+      dispatch(getAllProduct());
       console.log(newPenjualan);
     } catch (error) {
       console.log(error.response.data);
     }
-  }
+  };
 }
