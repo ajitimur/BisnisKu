e855b4bde461e0e33c01f4f400242a6fb54cd0c6
AbiRaditya <abi.radithya@gmail.com>
2021-10-20 17:30:36 +0800

    reactive , disabled select if quantity <= 0

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,11 +1,10 @@
-export { default as AddModal } from "./HomeScreen/AddModal"
-export { default as StackNavigationBox } from "./HomeScreen/StackNavigationBox"
-export { default as FinancialStatementBox } from "./HomeScreen/FinancialStatementBox"
-export { default as InformationBox } from "./HomeScreen/InformationBox"
+export { default as AddModal } from "./HomeScreen/AddModal";
+export { default as StackNavigationBox } from "./HomeScreen/StackNavigationBox";
+export { default as FinancialStatementBox } from "./HomeScreen/FinancialStatementBox";
+export { default as InformationBox } from "./HomeScreen/InformationBox";
 
-export { default as PenjualanForm } from "./TransaksiScreen/PenjualanForm"
-export { default as PengeluaranForm } from "./TransaksiScreen/PengeluaranForm"
-export { default as AddCustomer } from "./TransaksiScreen/AddCustomer"
+export { default as PenjualanForm } from "./TransaksiScreen/PenjualanForm";
+export { default as PengeluaranForm } from "./TransaksiScreen/PengeluaranForm";
 
-export { default as MainStatistikCard } from "./StatistikScreen/MainStatistikCard"
-export { default as StatistikChart } from "./StatistikScreen/StatistikChart"
+export { default as MainStatistikCard } from "./StatistikScreen/MainStatistikCard";
+export { default as StatistikChart } from "./StatistikScreen/StatistikChart";
